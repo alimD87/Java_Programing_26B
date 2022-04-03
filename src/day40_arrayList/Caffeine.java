89c402e6065bcef3898214a75db4e52d1c4eff06
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-03 11:51:03 -0400

    Day 40 Array_List

---
+++ src/day40_arrayList/Caffeine.java
@@ -0,0 +1,29 @@
+package day40_arrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Caffeine {
+    public static void main(String[] args) {
+
+        ArrayList<String> drinks = new ArrayList<>();
+        drinks.add("coffee");
+        drinks.add("tea");
+        drinks.add("energy drink");
+        drinks.add("soda");
+        System.out.println(drinks);
+
+        ArrayList<String> drinks2 = new ArrayList<>(drinks);
+        System.out.println(drinks2);
+
+        String [] allDrinks = {"coffee", "tea", "energy drink", "soda"};
+        //ArrayList<String> drinks3 = new ArrayList<>(allDrinks); not valid because the array is not a valid argument
+        ArrayList<String> drinks3 = new ArrayList<>(Arrays.asList(allDrinks));
+        System.out.println(drinks3);
+
+        ArrayList<String> drinks4 = new ArrayList<>(Arrays.asList("coffee", "tea", "energy drink", "soda"));
+        System.out.println(drinks4);
+
+
+    }
+}
