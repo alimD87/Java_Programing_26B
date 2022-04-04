05c9a2acde0a9e72b0cd5ee2afdc5316e5d75300
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-04 19:01:36 -0400

    Avengers Hours

---
+++ avangerHours/day1_04_04_2022/StringsRecap.java
@@ -0,0 +1,18 @@
+package day1_04_04_2022;
+
+public class StringsRecap {
+    /*
+    String -> object type. sequence of chars.
+    String class is a member of java.lang package. no need to import.
+    There are 2 ways to create string object;
+    1- String literal (mostly used) - String str = "Apple";
+    created in string pool in heap memory
+    2- new keyword - String str2 = new String ("Apple");
+    created in heap directly
+    **both are same in terms of usage, but only differs for memory allocation.
+    String objects are immutable which means unmodifiable/unchangeable.
+    String a = "apple"; // a new obj is created in str pool in heap
+    a = "Apple"; //a new obj is created in str pool in heap, not modified from the former version.
+    String methods --> practice!!!!!
+     */
+}
