11167354a677a5d82de16b339663a021f7bfe2e8
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-04 21:51:44 -0400

    Day 41 ArrayList

---
+++ src/Day41_arraylist/RemoveCountries.java
@@ -0,0 +1,27 @@
+package Day41_arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveCountries {
+     /*
+    Remove Countries
+Create a method that will take an ArrayList of countries and returns an ArrayList of countries that have a length of less than 7
+@param nums - The given ArrayList of numbers @return - ArrayList of numbers
+Ex:
+Input: "Japan", "Korea", "United States", "Turkey", "United Kingdom", "Canada"
+Output: "Japan", "Korea", "Turkey", "Canada"
+     */
+
+    public static ArrayList<String> removeCountries(ArrayList<String> countries){
+        countries.removeIf( country -> country.length() >= 7);
+        return countries;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> countries = new ArrayList<>(Arrays.asList("Japan", "Korea", "United States", "Turkey", "United Kingdom", "Canada"));
+        System.out.println(removeCountries(countries));
+
+    }
+}
