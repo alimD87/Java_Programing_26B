68e6f3c02b946acbff2ce40fed3c1dfb0984fb98
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-04 21:02:59 -0400

    Day 41 ArrayList

--- src/Day41_arraylist/SwitchBadPairs.java
+++ src/Day41_arraylist/SwitchBadPairs.java
@@ -2,7 +2,22 @@ package Day41_arraylist;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+/*
+   Given an ArrayList of words switch position for
+   each pair in the List. A pair is an element and
+   the element next to it.
+   The given ArrayList will always have an even
+   number of elements so each element will
+   always have a single pair
 
+   Ex:
+   Input: {"Cat", "in", "the", "hat"}
+   There is two pairs:
+   "Cat" and "in"
+   "the" and "hat"
+   Output: {"in", "Cat", "hat",
+   "the”}
+    */
 public class SwitchBadPairs {
     public static void main(String[] args) {
 
