b48eb273b7a2080a927175c64d925597f5387599
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-04 21:45:54 -0400

    Day 41 ArrayList

---
+++ src/Day41_arraylist/RemoveLongString.java
@@ -0,0 +1,29 @@
+package Day41_arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveLongString {
+    /*
+    Remove Long Strings
+Create a method that will accept an ArrayList of Strings and an int. Remove any String elements that have less characters than the given number.
+Return the modified ArrayList of Strings
+@param list - Given ArrayList of Strings @param int - max number of characters @return - ArrayList of Strings
+Ex: {“one,”, “two”, “three, “four”, “five”, “six”} Max number: 4
+Output: {“three, “four”, “five”
+     */
+
+    public static ArrayList<String> keepLongString(ArrayList<String> list, int maxLength){
+
+        list.removeIf( word -> word.length() < maxLength);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five", "six"));
+        System.out.println(keepLongString(words, 4));
+
+    }
+}
