e0c00e69d6280250d26535d09ccff147abd03bee
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-10 14:57:55 -0400

    Day 43 Custom classes

---
+++ src/day43_custom_classes/UsingThisClass.java
@@ -0,0 +1,10 @@
+package day43_custom_classes;
+
+public class UsingThisClass {
+    public static void main(String[] args) {
+
+        ThisKeyword obj = new ThisKeyword(300);
+        System.out.println(obj.a);
+
+    }
+}
