73300199d414af3bcd87eeb323589e7198503e6a
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-10 12:31:41 -0400

    Day 43 Custom classes

---
+++ src/day43_custom_classes/Marker.java
@@ -0,0 +1,24 @@
+package day43_custom_classes;
+
+public class Marker {
+
+    String type;
+    String brand;
+    String color;
+
+    @Override
+    public String toString() {
+        return "Marker{" +
+                "type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public Marker(String inputType, String inputBrand, String inputColor){
+        type = inputType;
+        brand = inputBrand;
+        color = inputColor;
+    }
+
+}
---
+++ src/day43_custom_classes/UsingMarker.java
@@ -0,0 +1,10 @@
+package day43_custom_classes;
+
+public class UsingMarker {
+    public static void main(String[] args) {
+
+        Marker marker1 = new Marker("Dry Erase", "Expo", "Red");
+        System.out.println(marker1);
+
+}
+}
