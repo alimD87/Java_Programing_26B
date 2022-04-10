7d5702a50a92aabfbdb68e30ca75a4a8940bf976
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-10 15:11:29 -0400

    Day 43 Custom classes

---
+++ src/day43_custom_classes/Address.java
@@ -0,0 +1,24 @@
+package day43_custom_classes;
+
+public class Address {
+
+
+    String street;
+    String city;
+    String state;
+    String zipcode;
+
+//    public Address(String inputStreet, String inputCity, String inputState, String inputZipcode){
+//        street = inputStreet;
+//        city = inputCity;
+//        state = inputState;
+//        zipcode = inputZipcode;
+//    }
+
+    public Address(String street, String city, String state, String zipcode){
+        this.street = street; // assigning the value of the local street from the parameters to the instance variable street
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+}
