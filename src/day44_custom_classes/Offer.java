95d725cc1233e4fa0a34b701aeec37a115b2c440
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-11 20:52:47 -0400

    day44 Custom Classes

---
+++ src/day44_custom_classes/Offer.java
@@ -0,0 +1,63 @@
+package day44_custom_classes;
+
+public class Offer {
+
+      /*
+        create a class called Offer
+        - data:
+            location, company, salary, is full time, number of PTO
+    */
+
+    String company;
+    String location;
+    double salary;
+    boolean isFullTime;
+    int numberOfPTO;
+
+    /*
+        - constructor
+            - create a constructor that creates an Offer object with the company and location
+            - create a constructor that creates an Offer object with the company, location, and salary
+            - create a constructor that creates an Offer object with the company, location, salary, is full time, and number of PTO
+    */
+
+    public Offer(String company, String location) {
+        this.company = company;
+        this.location = location;
+    }
+
+    public Offer(String company, String location, double salary) {
+        this.company = company;
+        this.location = location;
+        this.salary = salary;
+    }
+
+    public Offer(String company, String location, double salary, boolean isFullTime, int numberOfPTO) {
+        this.company = company;
+        this.location = location;
+        this.salary = salary;
+        this.isFullTime = isFullTime;
+        this.numberOfPTO = numberOfPTO;
+    }
+
+    /*
+        - method:
+            toString()
+                print all the employees information
+     */
+
+    public String toString(){
+
+        String info = company + " located in " + location;
+
+        info += "\n " + (salary > 0 ? salary : "");
+
+        info += "\n " + (isFullTime ? "Full Time" : "Contract");
+
+        info += "\n " + (numberOfPTO > 0 ? numberOfPTO : "");
+
+        return info;
+
+    }
+
+}
