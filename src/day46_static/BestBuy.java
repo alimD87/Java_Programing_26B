9a37dc0125508765706d88cdfc17150bb1fff355
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-13 19:29:00 -0400

    day46_static

---
+++ src/day46_static/BestBuy.java
@@ -0,0 +1,14 @@
+package day46_static;
+
+public class BestBuy {
+
+    String location;
+
+    static String headquarters = "Richfield, Minnesota, United States";
+    static String day = "Wednesday";
+    static int numberOfComputers = 100;
+
+    public BestBuy(String location) {
+        this.location = location;
+    }
+}
