a94b76216a0ae6a7598539a265e4bcb159d48cdc
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-13 22:04:57 -0400

    day46_static

--- src/day46_static/cydeo/CydeoStudent.java
+++ src/day46_static/cydeo/CydeoStudent.java
@@ -1,5 +1,7 @@
 package day46_static.cydeo;
 
+import java.util.Arrays;
+
 public class CydeoStudent {
 
     String name;
@@ -12,12 +14,27 @@ public class CydeoStudent {
 
     static {
         batchNumber = 26;
-        instructors = new String[] {"Nadir", "Mehmet", "Austin", "Asun", "Saim"};
+        instructors = new String[] {"Nadir", "Mehmet", "Austin", "Aysun", "Saim"};
         schoolName = "Cydeo";
+        printInfo();
     }
 
     public CydeoStudent(String name, int groupNumber) {
         this.name = name;
         this.groupNumber = groupNumber;
+    }
+
+    public static void printInfo(){
+        System.out.println("School Name" + schoolName);
+        System.out.println("Batch Number "+ batchNumber);
+        System.out.println("Instructors "+ Arrays.toString(instructors));
+    }
+
+    @Override
+    public String toString() {
+        return "CydeoStudent{" +
+                "name='" + name + '\'' +
+                ", groupNumber=" + groupNumber +
+                '}';
     }
 }
