cab6ddc4a00a5c7df84c6df29d4838e53978ee2a
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-15 16:32:12 -0400

    day46_static

---
+++ src/day46_static/Computer.java
@@ -0,0 +1,43 @@
+package day46_static;
+/*
+Computer - static
+
+    class name: computer:
+        instance variables:
+            price, brand, color, ...
+
+        static variables: hasScreen, hasBattery, hasMemory
+
+        make objects of the class in a main method and access both instance and static members from proper references
+ */
+public class Computer {
+
+    double price;
+    String brand;
+    String color;
+
+    static boolean hasScreen;
+    static boolean hasBattery;
+    static boolean hasMemory;
+
+    static {
+        hasScreen=true;
+        hasBattery=true;
+        hasMemory=true;
+    }
+
+    public Computer(double price, String brand, String color){
+        this.price=price;
+        this.brand=brand;
+        this.color=color;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "price=" + price +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/day46_static/UseComputer.java
@@ -0,0 +1,26 @@
+package day46_static;
+
+public class UseComputer {
+    public static void main(String[] args) {
+
+        Computer device1=new Computer(500,"Acer","Black");
+        Computer device2=new Computer(1100,"Apple","Silver");
+
+        //Accessing instance vxs-> called by object, belong to each object
+        System.out.println(device1.price);
+        System.out.println(device1.brand);
+        System.out.println(device1.color);
+
+        System.out.println(device2.price);
+        System.out.println(device2.brand);
+        System.out.println(device2.color);
+
+        //Accessing static vxs ->called by class, belong to the class
+        System.out.println(Computer.hasBattery);
+        System.out.println(Computer.hasMemory);
+        System.out.println(Computer.hasScreen);
+
+
+    }
+    }
+
