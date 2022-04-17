09a745519bee235e0be51a5034616e45dc9d9e85
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-17 18:53:47 -0400

    day47_encapsulation

---
+++ src/day47_encapsulation/Login.java
@@ -0,0 +1,25 @@
+package day47_encapsulation;
+
+public class Login {
+
+    private String username;
+    private String password;
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        if(password.length() >= 8){
+            this.password = password;
+        }
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
---
+++ src/day47_encapsulation/UseLogin.java
@@ -0,0 +1,24 @@
+package day47_encapsulation;
+
+public class UseLogin {
+    public static void main(String[] args) {
+
+        Login obj = new Login();
+//        obj.username = "jamesbond";
+//        obj.password = "1234";
+        // cannot access private variables directly
+
+        obj.setUsername("jamesbond");
+        obj.setPassword("1234");
+
+//        System.out.println(obj.username);
+//        System.out.println(obj.password);
+
+        System.out.println(obj.getUsername());
+        System.out.println(obj.getPassword());
+
+        obj.setPassword("james0007bond");
+        System.out.println(obj.getPassword());
+
+    }
+}
