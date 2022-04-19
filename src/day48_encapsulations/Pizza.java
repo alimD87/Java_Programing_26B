45457040f466f3f0bff1c1ab12355778f4d56a89
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-18 21:36:11 -0400

    day48_encapsulation

---
+++ src/day48_encapsulations/Pizza.java
@@ -0,0 +1,96 @@
+package day48_encapsulations;
+/*
+       create a class called Pizza
+
+       - data:
+
+           size (String), number of toppings
+
+       - constructor
+
+           - create a constructor that creates a Pizza object with the length and the width
+
+               call setters here
+
+       - encapsulate the Pizza class
+
+           - setter condition:
+               size can only be:
+                   small, medium, or large
+
+               number of toppings must be a positive number
+
+       - other methods methods:
+
+           - calculatePrice()
+                   calculate the price of the pizza based on:
+
+                       small base: $4
+                       medium base: $6
+                       large base: $8
+
+                       each topping adds $0.75 to the price
+
+                       calculate the total price and return the cost
+
+           - toString()
+               print the size, number of toppings, and cost of the pizza
+    */
+public class Pizza {
+
+    private String size;
+    private int numberOfToppings;
+
+    public Pizza(String size, int numberOfToppings){
+        setSize(size);
+        setNumberOfToppings(numberOfToppings);
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if(size.equalsIgnoreCase("small") || size.equalsIgnoreCase("medium") || size.equalsIgnoreCase("large")) {
+            this.size = size;
+        }
+    }
+
+    public int getNumberOfToppings() {
+        return numberOfToppings;
+    }
+
+    public void setNumberOfToppings(int numberOfToppings) {
+        if(numberOfToppings >= 0){
+            this.numberOfToppings = numberOfToppings;
+        }
+    }
+
+    /*
+
+            			small base: $4
+            			medium base: $6
+            			large base: $8
+
+            			each topping adds $0.75 to the price
+
+            			calculate the total price and return the cost
+
+     */
+
+    public double calculatePrice(){
+
+        if (size==null){
+            return  -1;
+        }
+
+        double price = size.equalsIgnoreCase("small") ? 4 : size.equalsIgnoreCase("medium") ? 6 : 8;
+
+        return price + (numberOfToppings * 0.75);
+    }
+
+    public String toString(){
+        return "Pizza size: " + size + ", number of toppings: " + numberOfToppings + ", price: $" + calculatePrice();
+    }
+
+}
---
+++ src/day48_encapsulations/PizzaStore.java
@@ -0,0 +1,21 @@
+package day48_encapsulations;
+
+public class PizzaStore {
+    public static void main(String[] args) {
+
+        Pizza first = new Pizza("large",3);
+        System.out.println(first);
+
+        //first.setNumberOfToppings =2; we can't call this way
+        first.setNumberOfToppings(2);
+        System.out.println(first);
+
+        Pizza second = new Pizza("", -2);
+        System.out.println(second);
+
+        second.setSize("Small");
+        second.setNumberOfToppings(-3);
+        System.out.println(second);
+
+    }
+}
