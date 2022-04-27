a675120c550bedc14e2ee79afe899cdc266c5e58
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-26 21:12:19 -0400

    day51_Inheritance

--- src/day52_inheritance/final_example/UseFinalExample.java
+++ src/day52_inheritance/final_example/UseFinalExample.java
@@ -3,6 +3,8 @@ package day52_inheritance.final_example;
 public class UseFinalExample {
     public static void main(String[] args) {
 
+        //public class UseFinalExample extends FinalExample { this is not valid because the FinalExample class was final, so it cannot be inherited
+
         System.out.println(FinalExample.PLANET);
        // FinalExample.PLANET = "Mars";
 
@@ -10,8 +12,20 @@ public class UseFinalExample {
         System.out.println(obj.a);
         System.out.println(obj.b);
 
+        System.out.println();
+
         FinalExample obj2 = new FinalExample(100);
         System.out.println(obj2.b);
+        System.out.println(obj2.b);
+
+        /*
+
+        obj and obj2 were separate objects. Each object has its own copy of instance variables a and b, both are final, so they cannot be changed.
+
+        a was hardcoded to be 5 in the class
+        b was assigned in the constructor call
+         */
+
 
     }
 }
