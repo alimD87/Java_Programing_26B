f037ec0257f1762ae6baa04d6b9103eac96b2491
alimD87 <101231678+alimD87@users.noreply.github.com>
2022-04-27 22:06:23 -0400

    day53_abstraction

--- src/day53_abstraction/exercise/Gym.java
+++ src/day53_abstraction/exercise/Gym.java
@@ -3,6 +3,8 @@ package day53_abstraction.exercise;
 public class Gym {
     public static void main(String[] args) {
 
+        //        Exercise obj1 = new Exercise(""); this class is abstract so you can't create an object
+
         Running obj2 = new Running ();
         System.out.println(obj2.name);
         obj2.performExercise();
